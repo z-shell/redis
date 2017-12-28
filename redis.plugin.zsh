7fa97eb856596562090b52b154e07b6ea702f5dd
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-12-28 17:12:09 +0100

    plugin.zsh: Redirect redis-server into a log file; a better message

--- redis.plugin.zsh
+++ redis.plugin.zsh
@@ -13,7 +13,7 @@
 typeset -g ZSRV_DIR="${0:h}"
 typeset -g ZSRV_PID
 
-local pidfile="$ZSRV_WORK_DIR"/"$ZSRV_ID".pid
+local pidfile="$ZSRV_WORK_DIR"/"$ZSRV_ID".pid logfile="$ZSRV_WORK_DIR"/"$ZSRV_ID".log
 local cfg="${ZSRV_DIR}/redis.conf"
 [[ ! -f "$cfg" ]] && cfg="${ZSRV_DIR}/redis.conf.default"
 
@@ -21,7 +21,7 @@ if [[ -f "$cfg" ]]; then
     { local pid="$(<$pidfile)"; } 2>/dev/null
     if [[ ${+commands[pkill]} = 1 && "$pid" = <-> && $pid -gt 0 ]]; then
         if pkill -INT -x -F "$pidfile" redis-server.\*; then
-            print "Stopped redis-server, PID: $pid"
+            print "Stopped previous redis-server instance, PID: $pid"
             LANG=C sleep 0.3
         else
             print "Previous redis-server instance (PID:$pid) not running"
@@ -29,7 +29,7 @@ if [[ -f "$cfg" ]]; then
     fi
 
     trap 'kill -INT $ZSRV_PID; exit 0' HUP
-    redis-server "$cfg" &; ZSRV_PID=$!
+    redis-server "$cfg" >>!"$logfile" 2>&1 &; ZSRV_PID=$!
     echo "$ZSRV_PID" >! "$pidfile"
     return 0
 else
